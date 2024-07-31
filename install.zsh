97907aca90fa93ba43097d30c6683a2e6a374c04
philips <130921787+philipstuessel@users.noreply.github.com>
2024-07-31 20:16:32 +0200

    fix: install.zsh

--- install.zsh
+++ install.zsh
@@ -2,6 +2,7 @@ source ~/.zshrc
 name="pin"
 folder="${JAP_FOLDER}plugins/packages/${name}/"
 folder_config="${folder}config/"
+md $folder_config
 fetch2 $folder "https://raw.githubusercontent.com/philipstuessel/$name/main/$name.zsh"
 echo {} > "${folder_config}pins.json"
 echo "--PIN is installed--"
