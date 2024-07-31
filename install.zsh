19f75a8ffd46599a16d17ce4349212d0759c8d24
philips <130921787+philipstuessel@users.noreply.github.com>
2024-07-31 20:48:03 +0200

    fix: install.zsh move mkdir

--- install.zsh
+++ install.zsh
@@ -2,7 +2,7 @@ source ~/.zshrc
 name="pin"
 folder="${JAP_FOLDER}plugins/packages/${name}/"
 folder_config="${folder}config/"
-mkdir $folder_config
 fetch2 $folder "https://raw.githubusercontent.com/philipstuessel/$name/main/$name.zsh"
+mkdir $folder_config
 echo {} > "${folder_config}pins.json"
 echo "--PIN is installed--"
